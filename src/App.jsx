951dabd0e57315742a6bb6bb9978bc4f376768bc
Charly1616 <josecarlosbermudezcastro45@gmail.com>
2025-05-01 23:09:24 -0500

    frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,40 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+const buttons = [
+  "|x|", "AC", "i", "+/-", "/",
+  "√x", "7", "8", "9", "x",
+  "^", "4", "5", "6", "-",
+  "Ln", "1", "2", "3", "+",
+  "↩", "DEL", "0", ".", "="
+];
+
 function App() {
+  const [theButtons] = useState(buttons)
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="backg">
+      <div className="calc">
+        <div className="calc-ans">
+          <p className="calc-prevOp">
+            (1+4i) x (4+3i)
+          </p>
+          <p className="calc-currOp">
+            -8+19i
+          </p>
+        </div>
+        <div className="buttons">
+          {theButtons.map( (e,i) => {
+            if (i===0 || i===4) return <button className='CalcButton sides top' key={i}>{e}</button>
+            if (i===20 || i===24) return <button className='CalcButton sides bottom' key={i}>{e}</button>
+            if (i%5===0 || i%5 === 4) return <button className='CalcButton sides' key={i}>{e}</button>
+            else return <button className='CalcButton' key={i}>{e}</button>
+          })}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      
+    </div>
   )
 }
 
